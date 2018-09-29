8c70bb54843aee645efe281e5c45d69ed3008177
Evan Odell <evanodell91@gmail.com>
2018-09-29 20:24:50 +0100

    moving files, adding new plots

--- R/8_stm_creation.R
+++ R/8_stm_creation.R
@@ -6,6 +6,9 @@ library(stm)
 library(quanteda)
 library(quanteda.corpora)
 library(readr)
+library(viridis)
+library(dplyr)
+library(ggplot2)
 
 lab_corpus <- read_rds("data/lab_corpus.rds")
 
@@ -20,13 +23,20 @@ lab_corpus_stm <- dfm(lab_corpus, remove_punct = TRUE,
 
 lab_corpus_stm$meta$short_list <- as.factor(lab_corpus_stm$meta$short_list)
 
+lab_corpus_stm$meta$gender <- as.factor(lab_corpus_stm$meta$gender)
+
+lab_corpus_stm$meta$gender <- factor(lab_corpus_stm$meta$gender,
+                                    levels=c("Male", "Female"))
+
+lab_corpus_stm$meta$gender_sl <- factor(
+  paste0(lab_corpus_stm$meta$gender, "_", lab_corpus_stm$meta$short_list),
+  levels=c("Male_FALSE", "Female_FALSE", "Female_TRUE")
+  )
+
 write_rds(lab_corpus_stm, "data/lab_corpus_stm.rds")
 
 
 # stm-creation-k30 -------------------------------------------------------
-
-library(stm)
-library(readr)
 
 lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
 
@@ -34,7 +44,7 @@ set.seed(402)
 #topic_model_k30 seed = 9957388
 topic_model_k30_m <- stm(lab_corpus_stm$documents, 
                          vocab = lab_corpus_stm$vocab, 
-                         K = 30, prevalence = ~short_list+gender, 
+                         K = 30, prevalence = ~gender_sl, 
                          seed = 9957388,
                          data = lab_corpus_stm$meta,
                          verbose = TRUE, init.type = "Spectral")
@@ -46,7 +56,7 @@ set.seed(402)
 #topic_model_k30 seed = 9957388
 topic_model_k45_m <- stm(lab_corpus_stm$documents, 
                          vocab = lab_corpus_stm$vocab, 
-                         K = 45, prevalence = ~short_list+gender, 
+                         K = 45, prevalence = ~gender_sl, 
                          seed = 9957388,
                          data = lab_corpus_stm$meta,
                          verbose = TRUE, init.type = "Spectral")
@@ -58,7 +68,7 @@ set.seed(402)
 #topic_model_k30 seed = 9957388
 topic_model_k60_m <- stm(lab_corpus_stm$documents, 
                          vocab = lab_corpus_stm$vocab, 
-                         K = 60, prevalence = ~short_list+gender, 
+                         K = 60, prevalence = ~gender_sl, 
                          seed = 9957388,
                          data = lab_corpus_stm$meta,
                          verbose = TRUE, init.type = "Spectral")
@@ -67,18 +77,35 @@ write_rds(topic_model_k60_m, "data/topic_model_k60_m.rds")
 
 # stm-creation-k0 -------------------------------------------------------
 
-set.seed(402)
-#topic_model_k30 seed = 9957388
-topic_model_k0_m <- stm(lab_corpus_stm$documents, 
-                        vocab = lab_corpus_stm$vocab, 
-                        K = 0, prevalence = ~gender + short_list, 
-                        seed = 9957388,
-                        data = lab_corpus_stm$meta,
-                        verbose = TRUE, init.type = "Spectral")
+# set.seed(402)
+# #topic_model_k30 seed = 9957388
+# topic_model_k0_m <- stm(lab_corpus_stm$documents, 
+#                         vocab = lab_corpus_stm$vocab, 
+#                         K = 0, prevalence = ~gender + short_list, 
+#                         seed = 9957388,
+#                         data = lab_corpus_stm$meta,
+#                         verbose = TRUE, init.type = "Spectral")
+# 
+# write_rds(topic_model_k0_m, "data/topic_model_k0_m.rds")
+# 
+# topic_model_k0_m2 <- stm(lab_corpus_stm$documents,
+#                          vocab = lab_corpus_stm$vocab,
+#                          K = 0, prevalence = ~gender_sl,
+#                          seed = 9957388,
+#                          data = lab_corpus_stm$meta,
+#                          verbose = TRUE, init.type = "Spectral")
+# 
+# write_rds(topic_model_k0_m2, "data/topic_model_k0_m2.rds")
 
-write_rds(topic_model_k0_m, "data/topic_model_k0_m.rds")
-
-
+# topic_model_k0_m3 <- stm(lab_corpus_stm$documents,
+#                         vocab = lab_corpus_stm$vocab,
+#                         K = 0, prevalence = ~gender * short_list + s(year),
+#                         content = ~gender_sl,
+#                         seed = 9957388,
+#                         data = lab_corpus_stm$meta,
+#                         verbose = TRUE, init.type = "Spectral")
+# 
+# write_rds(topic_model_k0_m3, "data/topic_model_k0_m3.rds")
 
 # stm-creation-k80 -------------------------------------------------------
 
@@ -86,7 +113,7 @@ set.seed(402)
 #topic_model_k30 seed = 9957388
 topic_model_k80_m <- stm(lab_corpus_stm$documents, 
                         vocab = lab_corpus_stm$vocab, 
-                        K = 80, prevalence = ~short_list+gender, 
+                        K = 80, prevalence = ~gender_sl, 
                         seed = 9957388,
                         data = lab_corpus_stm$meta,
                         verbose = TRUE, init.type = "Spectral")
@@ -99,72 +126,83 @@ set.seed(402)
 #topic_model_k30 seed = 9957388
 topic_model_k100_m <- stm(lab_corpus_stm$documents, 
                          vocab = lab_corpus_stm$vocab, 
-                         K = 100, prevalence = ~short_list+gender, 
+                         K = 100, prevalence = ~gender_sl, 
                          seed = 9957388,
                          data = lab_corpus_stm$meta,
                          verbose = TRUE, init.type = "Spectral")
 
 write_rds(topic_model_k100_m, "data/topic_model_k100_m.rds")
 
+rm(list=ls())
+gc()
+
 # corr-creation-k30 -------------------------------------------------------
 
-library(viridis)
-library(dplyr)
-library(igraph)
-library(ggplot2)
-library(quanteda)
-topic_model_k30 <- read_rds("data/topic_model_k30_m.rds")
-
-corr_topic_k30 <- topicCorr(topic_model_k30, method = "huge", verbose = FALSE)
-
-prep_k30 <- estimateEffect(1:30 ~ short_list + gender, topic_model_k30, 
-                           meta = lab_corpus_stm$meta, uncertainty = "Global")
-
-write_rds(corr_topic_k30, "data/corr_topic_k30_m.rds")
-
-write_rds(prep_k30, "data/prep_k30_m.rds")
+# topic_model_k30 <- read_rds("data/topic_model_k30_m.rds")
+# 
+# lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
+# 
+# corr_topic_k30 <- topicCorr(topic_model_k30, method = "huge", verbose = FALSE)
+# 
+# prep_k30 <- estimateEffect(~gender_sl, topic_model_k30, 
+#                            meta = lab_corpus_stm$meta, uncertainty = "Global")
+# 
+# write_rds(corr_topic_k30, "data/corr_topic_k30_m.rds")
+# 
+# write_rds(prep_k30, "data/prep_k30_m.rds")
 
 # corr-creation-k45 -------------------------------------------------------
 
-topic_model_k45 <- read_rds("data/topic_model_k45_m.rds")
-
-corr_topic_k45 <- topicCorr(topic_model_k45, method = "huge", verbose = FALSE)
-
-prep_k45 <- estimateEffect(1:45 ~ short_list + gender, topic_model_k45, 
-                           meta = lab_corpus_stm$meta, uncertainty = "Global")
-
-write_rds(corr_topic_k45, "data/corr_topic_k45_m.rds")
-
-write_rds(prep_k45, "data/prep_k45_m.rds")
+# rm(list=ls())
+# gc()
+# lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
+# 
+# 
+# topic_model_k45 <- read_rds("data/topic_model_k45_m.rds")
+# 
+# corr_topic_k45 <- topicCorr(topic_model_k45, method = "huge", verbose = FALSE)
+# 
+# prep_k45 <- estimateEffect( ~gender_sl, topic_model_k45, 
+#                            meta = lab_corpus_stm$meta, uncertainty = "Global")
+# 
+# write_rds(corr_topic_k45, "data/corr_topic_k45_m.rds")
+# 
+# write_rds(prep_k45, "data/prep_k45_m.rds")
 
 # corr-creation-k60 -------------------------------------------------------
 
-topic_model_k60 <- read_rds("data/topic_model_k60_m.rds")
-
-corr_topic_K60 <- topicCorr(topic_model_k60, method = "huge", verbose = FALSE)
-
-prep_K60 <- estimateEffect(1:60 ~ short_list + gender, topic_model_k60, 
-                           meta = lab_corpus_stm$meta, uncertainty = "Global")
-
-write_rds(corr_topic_K60, "data/corr_topic_K60_m.rds")
-
-write_rds(prep_K60, "data/prep_K60_m.rds")
+# rm(list=ls())
+# gc()
+# lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
+# 
+# topic_model_k60 <- read_rds("data/topic_model_k60_m.rds")
+# 
+# corr_topic_K60 <- topicCorr(topic_model_k60, method = "huge", verbose = FALSE)
+# 
+# prep_K60 <- estimateEffect(~gender_sl, topic_model_k60, 
+#                            meta = lab_corpus_stm$meta, uncertainty = "Global")
+# 
+# write_rds(corr_topic_K60, "data/corr_topic_K60_m.rds")
+# 
+# write_rds(prep_K60, "data/prep_K60_m.rds")
 
 
 # corr-creation-k0 -------------------------------------------------------
-
-
-lab_corpus_stm$meta$gender <- as.factor(lab_corpus_stm$meta$gender)
-
-lab_corpus_stm$meta$gender = factor(lab_corpus_stm$meta$gender, 
-                                    levels=c("Male", "Female"))
+rm(list=ls())
+gc()
+lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
 
 topic_model_k0 <- read_rds("data/topic_model_k0_m.rds")
 
 corr_topic_k0 <- topicCorr(topic_model_k0, method = "huge", verbose = FALSE)
 
-prep_k0 <- estimateEffect(~ gender+short_list, topic_model_k0, 
+prep_k0 <- estimateEffect(~ gender_sl, topic_model_k0, 
                           meta = lab_corpus_stm$meta, uncertainty = "Global")
+
+prep_k0_gender <- estimateEffect(~ gender, topic_model_k0, 
+                          meta = lab_corpus_stm$meta, uncertainty = "Global")
+
+write_rds(prep_k0_gender, "data/prep_k0_gender.rds")
 
 write_rds(corr_topic_k0, "data/corr_topic_k0_m.rds")
 
@@ -172,70 +210,76 @@ write_rds(prep_k0, "data/prep_k0_m.rds")
 
 # corr-creation-k80 -------------------------------------------------------
 
-topic_model_k80 <- read_rds("data/topic_model_k80_m.rds")
-
-corr_topic_k80 <- topicCorr(topic_model_k80, method = "huge", verbose = FALSE)
-
-prep_k80 <- estimateEffect(1:80 ~ short_list + gender, topic_model_k80, 
-                          meta = lab_corpus_stm$meta, uncertainty = "Global")
-
-write_rds(corr_topic_k80, "data/corr_topic_k80_m.rds")
-
-write_rds(prep_k80, "data/prep_k80_m.rds")
+# rm(list=ls())
+# gc()
+# lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
+# 
+# topic_model_k80 <- read_rds("data/topic_model_k80_m.rds")
+# 
+# corr_topic_k80 <- topicCorr(topic_model_k80, method = "huge", verbose = FALSE)
+# 
+# prep_k80 <- estimateEffect(~gender_sl, topic_model_k80, 
+#                           meta = lab_corpus_stm$meta, uncertainty = "Global")
+# 
+# write_rds(corr_topic_k80, "data/corr_topic_k80_m.rds")
+# 
+# write_rds(prep_k80, "data/prep_k80_m.rds")
 
 # corr-creation-k100 -------------------------------------------------------
 
+
+# lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
+# 
+# topic_model_k100 <- read_rds("data/topic_model_k100_m.rds")
+# 
+# corr_topic_k100 <- topicCorr(topic_model_k100, method = "huge", verbose = FALSE)
+# 
+# prep_k100 <- estimateEffect(~gender_sl, topic_model_k100, 
+#                           meta = lab_corpus_stm$meta, uncertainty = "Global")
+# 
+# write_rds(corr_topic_k100, "data/corr_topic_k100_m.rds")
+# 
+# write_rds(prep_k100, "data/prep_k100_m.rds")
+
+rm(list=ls())
+gc()
+
+library(purrr)
+topic_model_k30 <- read_rds("data/topic_model_k30_m.rds")
+
+topic_model_k45 <- read_rds("data/topic_model_k45_m.rds")
+
+topic_model_k60 <- read_rds("data/topic_model_k60_m.rds")
+
+topic_model_k0 <- read_rds("data/topic_model_k0_m.rds")
+
+topic_model_k80 <- read_rds("data/topic_model_k80_m.rds")
+
 topic_model_k100 <- read_rds("data/topic_model_k100_m.rds")
 
-corr_topic_k100 <- topicCorr(topic_model_k100, method = "huge", verbose = FALSE)
-
-prep_k100 <- estimateEffect(1:100 ~ short_list + gender, topic_model_k100, 
-                          meta = lab_corpus_stm$meta, uncertainty = "Global")
-
-
-
-
-write_rds(corr_topic_k100, "data/corr_topic_k100_m.rds")
-
-write_rds(prep_k100, "data/prep_k100_m.rds")
-
-
-# validity plot -----------------------------------------------------------
-
-many_models <- data_frame(K = c("30", "45", "60", "66", "80", "100"),
-                          topic_model = list(topic_model_k30,
-                                          topic_model_k45,
-                                          topic_model_k60,
-                                          topic_model_k0,
-                                          topic_model_k80,
-                                          topic_model_k100))
+lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
 
 heldout <- make.heldout(lab_corpus_stm$documents, lab_corpus_stm$vocab)
 
-library(purrr)
+#30 validity --------
 
-#checkResiduals(topic_model_k30, lab_corpus_stm$documents,  tol= 0.01)
-
-
-#30  --------
-
-exclusivity_k30 <- exclusivity(topic_model_k30)
-
-semantic_coherence_k30 <- semanticCoherence(topic_model_k30, lab_corpus_stm$documents)
-
-check_residuals_k30 <- checkResiduals(topic_model_k30, lab_corpus_stm$documents)
-
-bound_k30 <- max(topic_model_k30$convergence$bound)
-
-lfact_k30 <- lfactorial(topic_model_k30$settings$dim$K)
+# exclusivity_k30 <- exclusivity(topic_model_k30)
+#
+# semantic_coherence_k30 <- semanticCoherence(topic_model_k30, lab_corpus_stm$documents)
+# 
+# check_residuals_k30 <- checkResiduals(topic_model_k30, lab_corpus_stm$documents)
+# 
+# bound_k30 <- max(topic_model_k30$convergence$bound)
+# 
+# lfact_k30 <- lfactorial(topic_model_k30$settings$dim$K)
 
 eval_heldout_k30 <-  eval.heldout(topic_model_k30, heldout$missing)
 
-#45  --------
+#45 validity --------
 
-exclusivity_k45 <- exclusivity(topic_model_k45)
+#exclusivity_k45 <- exclusivity(topic_model_k45)
 
-semantic_coherence_k45 <- semanticCoherence(topic_model_k45, lab_corpus_stm$documents)
+#semantic_coherence_k45 <- semanticCoherence(topic_model_k45, lab_corpus_stm$documents)
 
 check_residuals_k45 <- checkResiduals(topic_model_k45, lab_corpus_stm$documents)
 
@@ -245,9 +289,7 @@ lfact_k45 <- lfactorial(topic_model_k45$settings$dim$K)
 
 eval_heldout_k45 <-  eval.heldout(topic_model_k45, heldout$missing)
 
-
-
-# 60  --------
+# 60 validity --------
 
 exclusivity_k60 <- exclusivity(topic_model_k60)
 
@@ -261,7 +303,7 @@ lfact_k60 <- lfactorial(topic_model_k60$settings$dim$K)
 
 eval_heldout_k60 <-  eval.heldout(topic_model_k60, heldout$missing)
 
-#0  --------
+#0 validity --------
 
 exclusivity_k0 <- exclusivity(topic_model_k0)
 
@@ -275,7 +317,7 @@ lfact_k0 <- lfactorial(topic_model_k0$settings$dim$K)
 
 eval_heldout_k0 <-  eval.heldout(topic_model_k0, heldout$missing)
 
-# 80  --------
+# 80 validity --------
 
 exclusivity_k80 <- exclusivity(topic_model_k80)
 
@@ -290,7 +332,8 @@ lfact_k80 <- lfactorial(topic_model_k80$settings$dim$K)
 eval_heldout_k80 <-  eval.heldout(topic_model_k80, heldout$missing)
 
 
-#100 --------
+#100 validity --------
+
 exclusivity_k100 <- exclusivity(topic_model_k100)
 
 semantic_coherence_k100 <- semanticCoherence(topic_model_k100, lab_corpus_stm$documents)
@@ -302,6 +345,8 @@ bound_k100 <- max(topic_model_k100$convergence$bound)
 lfact_k100 <- lfactorial(topic_model_k100$settings$dim$K)
 
 eval_heldout_k100 <-  eval.heldout(topic_model_k100, heldout$missing)
+
+library(tibble)
 
 k_models <- data_frame(K = c(30, 45, 60, 66, 80, 100),
                        exclusivity = list(exclusivity_k30,
@@ -345,32 +390,3 @@ k_models$lbound <- k_models$bound + k_models$lfact
 
 write_rds(k_models, "data/k_models.rds")
 
-library(tidyr)
-
-
-# library(stm)
-# library(readr)
-# library(purrr)
-# library(dplyr)
-# library(tibble)
-# 
-# lab_corpus_stm <- read_rds("data/lab_corpus_stm.rds")
-# 
-# 
-# model_check <- data_frame(K = c(30, 50, 75, 100)) %>%
-#   mutate(topic_model = ~stm(documents = lab_corpus_stm$documents, 
-#                             vocab = lab_corpus_stm$vocab, K = .,
-#                             prevalence = ~short_list+gender, 
-#                             data = lab_corpus_stm$meta,
-#                             verbose = TRUE))
-# 
-# search_results_m <- searchK(documents = lab_corpus_stm$documents, 
-#                             vocab = lab_corpus_stm$vocab, 
-#                             K = c(30, 50, 75, 100),
-#                             prevalence = ~short_list+gender, 
-#                             data = lab_corpus_stm$meta,
-#                             verbose = TRUE)
-# 
-# write_rds(search_results_m, "data/search_results_m.rds")
-# 
-# search_results_m
---
+++ R/9_stm_effect.R
@@ -0,0 +1,108 @@
+#devtools::install_github("mikaelpoul/tidystm", dependencies = TRUE)
+library(tidystm)
+library(ggplot2)
+library(dplyr)
+effect <-  extract.estimateEffect(x = prep_k0,
+                                  covariate = "gender_sl",
+                                  method = "pointestimate",
+                                  model = topic_model_k0)
+
+effect$label <- gsub("\\(Covariate Level: Male_FALSE\\)", "", effect$label)
+
+effect <- effect %>% mutate(label = recode(label, 
+                                           "Topic 1" = "(1) Employment & unions",
+                                           "Topic 2" = "(2) Legal system",
+                                           "Topic 3" = "(3) Roads",
+                                           "Topic 4" = "(4) Housing",
+                                           "Topic 5" = "(5) Police, firefighters & prison",
+                                           "Topic 6" = "(6) Northern Ireland",
+                                           "Topic 7" = "(7) Committee",
+                                           "Topic 8" = "(8) Schools",
+                                           "Topic 9" = "(9) Energy & climate change",
+                                           "Topic 10" = "(10) Defence",
+                                           "Topic 11" = "(11) Parliament",
+                                           "Topic 12" = "(12) International politics",
+                                           "Topic 13" = "(13) Ministers",
+                                           "Topic 14" = "(14) Policy impact",
+                                           "Topic 15" = "(15) Gender",
+                                           "Topic 16" = "(16) Regional development",
+                                           "Topic 17" = "(17) Communications",
+                                           "Topic 18" = "(18) Immigration",
+                                           "Topic 19" = "(19) Health system",
+                                           "Topic 20" = "(20) International development",
+                                           "Topic 21" = "(21) Benefits & disability",
+                                           "Topic 22" = "(22) Sport & culture",
+                                           "Topic 23" = "(23) History",
+                                           "Topic 24" = "(24) Higher education & skills",
+                                           "Topic 25" = "(25) Concurring point",
+                                           "Topic 26" = "(26) Pensions",
+                                           "Topic 27" = "(27) Points of order",
+                                           "Topic 28" = "(28) Issues",
+                                           "Topic 29" = "(29) Constituencies",
+                                           "Topic 30" = "(30) Ethnic groups & racism",
+                                           "Topic 31" = "(31) Amendments",
+                                           "Topic 32" = "(32) Reports",
+                                           "Topic 33" = "(33) People",
+                                           "Topic 34" = "(34) Wales & Scotland",
+                                           "Topic 35" = "(35) Alcohol & tobacco",
+                                           "Topic 36" = "(36) Place names",
+                                           "Topic 37" = "(37) Budget",
+                                           "Topic 38" = "(38) Tax",
+                                           "Topic 39" = "(39) Private companies",
+                                           "Topic 40" = "(40) Environment & fishing",
+                                           "Topic 41" = "(41) Crime",
+                                           "Topic 42" = "(42) Bills",
+                                           "Topic 43" = "(43) Children",
+                                           "Topic 44" = "(44) Utilities & PFI",
+                                           "Topic 45" = "(45) Middle East",
+                                           "Topic 46" = "(46) Local authorities",
+                                           "Topic 47" = "(47) Elections",
+                                           "Topic 48" = "(48) Debate",
+                                           "Topic 49" = "(49) Transport",
+                                           "Topic 50" = "(50) Questions",
+                                           "Topic 51" = "(51) Families",
+                                           "Topic 52" = "(52) Health research",
+                                           "Topic 53" = "(53) Dispatch box",
+                                           "Topic 54" = "(54) Parties",
+                                           "Topic 55" = "(55) Statements",
+                                           "Topic 56" = "(56) European Union",
+                                           "Topic 57" = "(57) Locations",
+                                           "Topic 58" = "(58) Jobs & manufacturing",
+                                           "Topic 59" = "(59) Small business",
+                                           "Topic 60" = "(60) Agreement & disagreement",
+                                           "Topic 61" = "(61) Voluntary sector",
+                                           "Topic 62" = "(62) Comments",
+                                           "Topic 63" = "(63) Social care",
+                                           "Topic 64" = "(64) Time",
+                                           "Topic 65" = "(65) Media & animals",
+                                           "Topic 66" = "(66) Other"))
+
+write_rds(effect, "data/effect.rds")
+
+
+t_list <- c(1:66)
+
+for (i in t_list){
+  p_effect <- ggplot(effect %>% filter(topic == i),
+                     aes(y = estimate, x = covariate.value,
+                         fill = covariate.value)) + 
+    geom_bar(stat = "identity", position = position_dodge()) +
+    geom_errorbar(aes(ymin = ci.lower, ymax = ci.upper), 
+                  color = "black", width = 0.3, position=position_dodge(.9)) + 
+    scale_y_continuous(labels = scales::percent) + 
+    scale_fill_manual(values = c("#a06dba", "#7aa457", "#cb6a49"),
+                      labels = c("Men", "Non-AWS", "AWS"),
+                      name = "") + 
+    scale_x_discrete(labels = c("Men", "Non-AWS", "AWS")) + 
+    labs(x = "",
+         y = "Expected Topic Proportion",
+         title = effect$label[effect$topic==i][[1]]) +
+    theme(legend.position = "bottom")
+  
+  ggsave(paste0("topic_", i, "_bar.svg"), plot = p_effect, 
+         path="plots/ee-plots", device = "svg", units = "cm",
+         width = 15, height = 20)
+}
+
+
+
